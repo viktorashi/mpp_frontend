387be8e8755aa366cacb2329487ada14c3f54951
viktorashi <33178074+viktorashi@users.noreply.github.com>
2024-05-20 12:26:47 +0300

    am incercat un backrnd

--- src/service/ElementApi.ts
+++ src/service/ElementApi.ts
@@ -1,6 +1,11 @@
 import Element from "../model/Element";
-//deci cand pun din server doar schimb ma gandesc linia asta cu sa vina in loc de din fisieru local sa vina de la server gien
-import { elements } from "./periodic-table.json";
-const elementList = elements as Element[];
+// import { elements } from "./periodic-table.json";
+import axios from "axios";
 
+let elementList: Element[] = [];
+
+axios.get("http://127.0.0.1:5000/getall").then((response) => {
+  elementList = response.data as Element[];
+});
+console.log(elementList);
 export default elementList;
